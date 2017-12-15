2dc8468c7f237ea7670a2a667454af4d53a3ea2e
Eric Broersma <git@ericbroersma.nl>
2017-12-15 12:26:15 +0100

    Day 13

---
+++ day13_2.py
@@ -0,0 +1,63 @@
+input = """0: 3
+1: 2
+2: 6
+4: 4
+6: 4
+8: 10
+10: 6
+12: 8
+14: 5
+16: 6
+18: 8
+20: 8
+22: 12
+24: 6
+26: 9
+28: 8
+30: 8
+32: 10
+34: 12
+36: 12
+38: 8
+40: 12
+42: 12
+44: 14
+46: 12
+48: 12
+50: 12
+52: 12
+54: 14
+56: 14
+58: 14
+60: 12
+62: 14
+64: 14
+66: 17
+68: 14
+72: 18
+74: 14
+76: 20
+78: 14
+82: 18
+86: 14
+90: 18
+92: 14""".split("\n")
+
+depth_ranges = {}
+
+for line in input:
+    depth, range = line.split(": ")
+    depth_ranges[int(depth)] = int(range)
+
+def do_run(delay):
+    for depth, range in depth_ranges.items():
+        time = delay + depth
+        if time % ((range-1) * 2) == 0:
+            return False
+    return True
+    
+delay = 0
+while not do_run(delay):
+    delay += 1
+        
+print(delay)
